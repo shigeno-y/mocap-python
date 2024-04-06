0f8095a14299c9fc7024bfe0b58397f13c1394de
shigeno-y <37774973+shigeno-y@users.noreply.github.com>
2024-04-06 13:46:04 +0900

    update parser for ipad
    i'm not sure representing IPv4 in decimal

--- mocap/Reader/SMF/types/sndf.py
+++ mocap/Reader/SMF/types/sndf.py
@@ -17,18 +17,35 @@ class sndf(DataBlock):
 class ipad(DataBlock):
     """IP Address Box"""
 
-    _FIELDS = "BBBBxxxx"
+    _FIELDS = "Q"
     _4CC = "ipad"
 
     def __init__(self, *, size: int, type: str, data: bytes):
         super().__init__(size=size, type=type, data=data)
 
     def _parseData(self):
-        # import ipaddress
+        import ipaddress
 
         self._readRAW()
+        self._parsed = self._parsed[0]
 
-        # self._parsed = ipaddress.ip_address(self._parsed)
+        v4 = list(
+            (
+                0,
+                0,
+                0,
+                0,
+            )
+        )
+        v4[3] = self._parsed % 1000
+        self._parsed //= 1000
+        v4[2] = self._parsed % 1000
+        self._parsed //= 1000
+        v4[1] = self._parsed % 1000
+        self._parsed //= 1000
+        v4[0] = self._parsed % 1000
+
+        self._parsed = ipaddress.IPv4Address(".".join([str(v) for v in v4]))
 
 
 class rcvp(DataBlock):
@@ -42,3 +59,4 @@ class rcvp(DataBlock):
 
     def _parseData(self):
         self._readRAW()
+        self._parsed = self._parsed[0]
